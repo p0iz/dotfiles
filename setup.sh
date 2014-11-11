5c7221771f5e334452e1ce1ea2187d6ebe3abe4c
Tommi <tommi.martela@gmail.com>
2014-11-11 20:47:44 +0200

    Add screen rc and make setup script generic

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-pushd $HOME
+# Get the directory where this script is located
+DFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# ZSH
-ln -s dotfiles/zshrc .zshrc
+pushd $HOME
+echo -n "Creating symlinks to dotfiles in home directory"
+
+for file in $(ls $DFDIR); do
+  if [ $file != README.md ]; then
+    ln -b -s $DFDIR/$file .$file
+    echo -n "."
+  fi
+done
+
+echo " done!"
 
 popd
